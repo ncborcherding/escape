f7257bf014e498fd7438d5504fb0b1916c2eaa38
Nick <nicholas-borcherding@uiowa.edu>
2020-06-10 12:28:57 -0500

    Final Countdown Checks
    Optimize function legtnh, indents, and line space for biocheck
    Changed example to run on 2 cores to prevent warning during R CMD check.

--- R/visualizations.R
+++ R/visualizations.R
@@ -136,11 +136,11 @@ masterPCAPlot <- function(enriched, PCx, PCy, top.contribution = 10) {
 ridgeEnrichment <- function(enriched, group = "cluster", gene.set = NULL, 
             scale.bracket = NULL, facet = NULL, add.rug = FALSE,
             colors = c("#0348A6", "#7AC5FF", "#C6FDEC", "#FFB433", "#FF4B20")) 
-             {
+            {
     if (!is.null(scale.bracket)) {
         if (length(scale.bracket) != 1 | length(scale.bracket) != 1) {
             stop("Please indicate one or two values for the scale.bracket 
-                 parameter, such as scale.bracket = c(-2,2)")
+                parameter, such as scale.bracket = c(-2,2)")
         }
         scale.bracket <- order(scale.bracket)
         if(length(scale.bracket) == 1) {
@@ -150,7 +150,7 @@ ridgeEnrichment <- function(enriched, group = "cluster", gene.set = NULL,
         tmp <- enriched
         tmp[,gene.set]<- scale(tmp[,gene.set])
         rows_selected <- rownames(tmp[tmp[,gene.set] >= scale.bracket[1] & 
-                                          tmp[,gene.set] <= scale.bracket[2],])
+                            tmp[,gene.set] <= scale.bracket[2],])
         enriched <- enriched[rownames(enriched) %in% rows_selected,]
     }
     colors <- assignColor(colors, enriched, group) 
@@ -205,7 +205,8 @@ GeomSplitViolin <- ggproto("GeomSplitViolin", GeomViolin,
                 both <- cbind(quantiles, aesthetics)
                 quantile_grob <- GeomPath$draw_panel(both, ...)
                 ggplot2:::ggname("geom_split_violin", 
-                    grid::grobTree(GeomPolygon$draw_panel(newdata, ...), quantile_grob))
+                    grid::grobTree(GeomPolygon$draw_panel(newdata, ...), 
+                            quantile_grob))
         } else {
             ggplot2:::ggname("geom_split_violin", 
                 GeomPolygon$draw_panel(newdata, ...))}
@@ -262,7 +263,7 @@ splitEnrichment <- function(enriched, x.axis = NULL, scale.bracket = NULL,
     if (!is.null(scale.bracket)) {
         if (length(scale.bracket) != 1 | length(scale.bracket) != 1) {
             stop("Please indicate one or two values for the scale.bracket 
-                 parameter, such as scale.bracket = c(-2,2)")
+                parameter, such as scale.bracket = c(-2,2)")
         }
         scale.bracket <- order(scale.bracket)
         if(length(scale.bracket) == 1) {
@@ -272,7 +273,7 @@ splitEnrichment <- function(enriched, x.axis = NULL, scale.bracket = NULL,
         tmp <- enriched
         tmp[,gene.set]<- scale(tmp[,gene.set])
         rows_selected <- rownames(tmp[tmp[,gene.set] >= scale.bracket[1] & 
-                                          tmp[,gene.set] <= scale.bracket[2],])
+                            tmp[,gene.set] <= scale.bracket[2],])
         enriched <- enriched[rownames(enriched) %in% rows_selected,]
     }
     colors <- assignColor(colors, enriched, split) 
@@ -285,8 +286,7 @@ splitEnrichment <- function(enriched, x.axis = NULL, scale.bracket = NULL,
                     y = enriched[,gene.set], 
                     fill = enriched[,split])) + 
             xlab(x.axis) 
-        check = NULL
-    }
+        check = NULL}
     plot <- plot + 
         geom_split_violin(alpha=0.8) +
         geom_boxplot(width=0.1, fill = "grey", alpha=0.5, 
@@ -299,8 +299,7 @@ splitEnrichment <- function(enriched, x.axis = NULL, scale.bracket = NULL,
     if (!is.null(check)) {
         plot <- plot + theme(axis.title.x = element_blank(),
                     axis.text.x = element_blank(),
-                    axis.ticks.x = element_blank())
-    }
+                    axis.ticks.x = element_blank())}
     return(plot)
 }
 
