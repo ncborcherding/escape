385c9c3b0bfffe4f9a56512da6317a72bde30ba2
theHumanBorch <ncborch@gmail.com>
2024-02-24 15:36:10 +0100

    Normalization now takes vectors

--- R/performNormalization.R
+++ R/performNormalization.R
@@ -12,6 +12,7 @@
 #' @param assay Name of the assay to plot if data is a single-cell object.
 #' @param gene.sets The gene set library to use to extract 
 #' the individual gene set information from.
+#' @param scale.factor A vector to use for normalizing enrichment scores per cell.
 #' @param make.positive Shift enrichment values to a positive range \strong{TRUE}
 #' for downstream analysis or not \strong{TRUE} (default).
 #' 
@@ -34,13 +35,18 @@
 performNormalization <- function(input.data,
                                  assay = NULL,
                                  gene.sets = NULL,
-                                 make.positive = FALSE) {
+                                 make.positive = FALSE,
+                                 scale.factor = NULL) {
   
   
   if(is_seurat_or_se_object(input.data)) {
     enriched <- .pull.Enrich(input.data, assay)
   } else {
     enriched <- input.data
+  }
+  
+  if(!is.null(scale.factor) & length(scale.factor) != dim(input.data)[2]) {
+    stop("If using a vector as a scale factor, please ensure the length matches the number of cells.")
   }
   
   #Getting the gene sets that passed filters
@@ -50,20 +56,29 @@ performNormalization <- function(input.data,
   
   #Isolating the number of genes per cell expressed
   cnts <- .cntEval(input.data, assay = "RNA", type = "counts")
-  egc.size <- lapply(egc, function(x)  {
-    lapply(seq_len(ncol(cnts)), function(y) {
-      values <-length(which(names(cnts[which(cnts[,y] != 0),y]) %in% x))
-      values
-    })
-  })
   
+  if(is.null(scale.factor)) {
+    print("Calculating features per cell...")
+    egc.size <- lapply(egc, function(x)  {
+        lapply(seq_len(ncol(cnts)), function(y) {
+          values <-length(which(names(cnts[which(cnts[,y] != 0),y]) %in% x))
+          values
+        })
+      })
+  }
+  
+  print("Normalizing enrichment scores per cell...")
   #Dividing the enrichment score by number of genes expressed
   lapply(seq_len(ncol(enriched)), function(x) {
-    gene.set <- unlist(egc.size[colnames(enriched)[x]])
-    if(any(gene.set == 0)) {
-      gene.set[which(gene.set == 0)] <- 1
+    if (!is.null(scale.factor)) {
+      enriched[,x] <- enriched[,x]/scale.factor
+    } else {
+      gene.set <- unlist(egc.size[colnames(enriched)[x]])
+      if(any(gene.set == 0)) {
+        gene.set[which(gene.set == 0)] <- 1
+      }
+      enriched[,x] <- enriched[,x]/gene.set
     }
-    enriched[,x] <- enriched[,x]/gene.set
     if(any(enriched[,x] < 0) & make.positive) {
       enriched[,x] <- enriched[,x] + abs(min(enriched[,x]))
     }
